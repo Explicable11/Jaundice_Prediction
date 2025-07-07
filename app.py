cb70e54c76cbec1ffcce6760494261a5fe4d664c
Dev Ayush <143614582+Explicable11@users.noreply.github.com>
2025-07-07 23:08:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import joblib
 from PIL import Image
 
-# --- flask App Setup ---
+# --- flask app Setup ---
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'bmp', 'tiff'}
